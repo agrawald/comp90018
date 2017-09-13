3c2765ce41027a4f7bbef7cb800ecb209ab69db3
lilianc2 <lilianc2@student.unimelb.edu.au>
2017-09-13 22:09:37 +1000

    Update MainActivity

---
+++ rfid-android-notifier/App/app/src/main/java/comp90018/autotag/NotificationSettings.java
@@ -0,0 +1,11 @@
+package comp90018.autotag;
+
+/**
+ * Created by Lilian on 12/09/2017.
+ */
+
+public class NotificationSettings {
+    public static String SenderID = "";
+    public static String HubName = "rfidNotificationHub";
+    public static String HubListenConnectionString = "Endpoint=sb://rfidnotificationsns.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=UJtMa+2kQCo0XuZQ4feEZn+4N4l197bA+UQOaCy+Ja4=";
+}
