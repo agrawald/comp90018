cf1c7b03af364381a43b121a7cfacf2849f2aa9e
Olivia <aoli.lady813@gmail.com>
2017-09-16 16:22:36 +1000

    Success in upload nfc data

---
+++ rfid-android-nfc-reader/app/src/main/java/com/mayi/rfid/nfcrfidreader/ToDoItemAdapter.java
@@ -0,0 +1,74 @@
+package com.mayi.rfid.nfcrfidreader;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+
+/**
+ * Created by Ao Li on 16/9/17.
+ */
+
+public class ToDoItemAdapter extends ArrayAdapter<ToDoItem> {
+
+    /**
+     * Adapter context
+     */
+    Context mContext;
+
+    /**
+     * Adapter View layout
+     */
+    int mLayoutResourceId;
+
+    public ToDoItemAdapter(Context context, int layoutResourceId) {
+        super(context, layoutResourceId);
+
+        mContext = context;
+        mLayoutResourceId = layoutResourceId;
+    }
+
+    /**
+     * Returns the view for a specific item on the list
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+
+        final ToDoItem currentItem = getItem(position);
+
+        if (row == null) {
+            LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+            row = inflater.inflate(mLayoutResourceId, parent, false);
+        }
+
+        row.setTag(currentItem);
+        final CheckBox checkBox = (CheckBox) row.findViewById(R.id.checkToDoItem);
+        checkBox.setText(currentItem.getText());
+        checkBox.setChecked(false);
+        checkBox.setEnabled(true);
+
+        checkBox.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                if (checkBox.isChecked()) {
+                    checkBox.setEnabled(false);
+                    if (mContext instanceof MainActivity) {
+                        MainActivity activity = (MainActivity) mContext;
+                        Log.d("!!!!!!",currentItem.getId());
+                        Log.d("!!!!!!",currentItem.getText());
+                        activity.checkItem(currentItem);
+                    }
+                }
+            }
+        });
+
+        return row;
+    }
+
+}
