426ad9abadf657a5b18e83132b61ee6126575214
dagrawal <agrawal.dheeraj.7@gmail.com>
2017-09-20 20:55:34 +1000

    Adding the logic to send auth notification back to rpi.

--- rfid-cs-nfc-backend/nfcConnectionService/Controllers/TodoItemController.cs
+++ rfid-cs-nfc-backend/nfcConnectionService/Controllers/TodoItemController.cs
@@ -6,14 +6,17 @@ using System.Web.Http.OData;
 using Microsoft.Azure.Mobile.Server;
 using NFCConnectionService.DataObjects;
 using NFCConnectionService.Models;
-
+using Microsoft.Azure.Devices;
+using System.Text;
 
 namespace NFCConnectionService.Controllers
 {
     public class TodoItemController : TableController<TodoItem>
     {
-       // const string connectionString = "MS_AzureStorageAccountConnectionString";
+        // const string connectionString = "MS_AzureStorageAccountConnectionString";
         //const string tableName = "TodoItems;
+        static ServiceClient serviceClient;
+        static string connectionString = "HostName=iotHubRFID.azure-devices.net;DeviceId=rfid_raspberry_pi;SharedAccessKey=+0+mQi58+whcEiUpLbU/yRT/jQymBNoi+h1oRvEb3bU=";
 
 
         protected override void Initialize(HttpControllerContext controllerContext)
@@ -21,6 +24,7 @@ namespace NFCConnectionService.Controllers
             base.Initialize(controllerContext);
             NFCConnectionContext context = new NFCConnectionContext();
             DomainManager = new EntityDomainManager<TodoItem>(context, Request);
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
         }
 
         // GET tables/TodoItem
@@ -48,6 +52,12 @@ namespace NFCConnectionService.Controllers
             return CreatedAtRoute("Tables", new { id = current.Id }, current);
         }
 
+        // POST rfid/authorized
+        private async static Task PostRfidAuthorized()
+        {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes("Cloud to device message."));
+            await serviceClient.SendAsync("rfid-raspberry-pi", commandMessage);
+        }
         // DELETE tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
         public Task DeleteTodoItem(string id)
         {
