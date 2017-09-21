6489d7ee6774db22825c2a0349770308cc6eab3e
Olivia <aoli.lady813@gmail.com>
2017-09-21 22:02:27 +1000

    refine addItem class

--- rfid-android-nfc-reader/app/src/main/java/com/mayi/rfid/nfcrfidreader/AutoTagActivity.java
+++ rfid-android-nfc-reader/app/src/main/java/com/mayi/rfid/nfcrfidreader/AutoTagActivity.java
@@ -4,25 +4,14 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.microsoft.windowsazure.mobileservices.*;
 import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
-
-import java.net.MalformedURLException;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-
-import static com.microsoft.windowsazure.mobileservices.table.query.QueryOperations.val;
 
 public class AutoTagActivity extends AppCompatActivity {
 
@@ -33,15 +22,14 @@ public class AutoTagActivity extends AppCompatActivity {
     private MobileServiceTable<AutoTag> autoTagTable;
 
     private NfcAdapter nfcAdapter;
-    TextView tvIdInfo;
-    TextView tvTextInfo;
+    TextView tvAutoTagId;
+    TextView tvAutoTagAuthorized;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvTextInfo = (TextView) findViewById(R.id.info);
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (nfcAdapter == null) {
             Toast.makeText(this,
@@ -59,12 +47,12 @@ public class AutoTagActivity extends AppCompatActivity {
             autoTagNFCClient = new MobileServiceClient(
                     "https://nfcconnection.azurewebsites.net",
                     this);
-            autoTagTable = autoTagNFCClient.getTable("AutoTag",AutoTag.class);
+            autoTagTable = autoTagNFCClient.getTable(AutoTag.class);
         }catch( Exception e){
             createAndShowDialog(e, "Error");
         }
-        tvIdInfo = (TextView) findViewById(R.id.tvIdToDo);
-        tvTextInfo = (TextView) findViewById(R.id.tvTextToDo);
+        tvAutoTagId = (TextView) findViewById(R.id.tvAutoTagId);
+        tvAutoTagAuthorized = (TextView) findViewById(R.id.tvAutoTagAuthorized);
     }
 
     @Override
@@ -81,7 +69,7 @@ public class AutoTagActivity extends AppCompatActivity {
 
             Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
             if (tag == null) {
-                tvIdInfo.setText("tag == null");
+                tvAutoTagId.setText("tag == null");
             } else {
                 int i, j, in;
                 String [] hex = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
@@ -98,8 +86,7 @@ public class AutoTagActivity extends AppCompatActivity {
                     i = in & 0x0f;
                     tagIdInfo += hex[i];
                 }
-                tvIdInfo.setText(tagIdInfo);
-                tvTextInfo.setText(tagTextInfo);
+                tvAutoTagId.setText(tagIdInfo);
             }
         } else {
             Toast.makeText(this,
@@ -119,32 +106,20 @@ public class AutoTagActivity extends AppCompatActivity {
         }
 
         // Create a new item
-        AutoTag nfcTag = new AutoTag();
-        nfcTag.setAuthorized(false);
-        nfcTag.setId(tvIdInfo.getText().toString());
+        AutoTag autoTag = new AutoTag();
+        autoTag.setAuthorized(false);
+        autoTag.setId(tvAutoTagId.getText().toString());
         try {
-            autoTagTable.insert(nfcTag).get();
+            autoTagTable.insert(autoTag);
         }catch( ExecutionException e){
 
         }catch (InterruptedException e){
 
         }
+        tvAutoTagId.setText("");
     }
 
-    private void createAndShowDialog(Exception exception, String title) {
-        Throwable ex = exception;
-        if(exception.getCause() != null){
-            ex = exception.getCause();
-        }
-        createAndShowDialog(ex.getMessage(), title);
-    }
-    private void createAndShowDialog(final String message, final String title) {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        builder.setMessage(message);
-        builder.setTitle(title);
-        builder.create().show();
-    }
     /**
      * Creates a dialog and shows it
      *
@@ -160,5 +135,38 @@ public class AutoTagActivity extends AppCompatActivity {
                 createAndShowDialog(exception, "Error");
             }
         });
+    }
+
+
+    /**
+     * Creates a dialog and shows it
+     *
+     * @param exception
+     *            The exception to show in the dialog
+     * @param title
+     *            The dialog title
+     */
+    private void createAndShowDialog(Exception exception, String title) {
+        Throwable ex = exception;
+        if(exception.getCause() != null){
+            ex = exception.getCause();
+        }
+        createAndShowDialog(ex.getMessage(), title);
+    }
+
+    /**
+     * Creates a dialog and shows it
+     *
+     * @param message
+     *            The dialog message
+     * @param title
+     *            The dialog title
+     */
+    private void createAndShowDialog(final String message, final String title) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(message);
+        builder.setTitle(title);
+        builder.create().show();
     }
 }
