85e9957fe5821e051b42ba5c7e4d2fc07296c555
lilianc2 <lilianc2@student.unimelb.edu.au>
2017-09-22 19:29:09 +1000

    Fixed Notifications Settings

--- rfid-android-notifier/app/src/main/java/au/uni/melb/rfid/notifier/NotificationSettings.java
+++ rfid-android-notifier/app/src/main/java/au/uni/melb/rfid/notifier/NotificationSettings.java
@@ -6,20 +6,9 @@ package au.uni.melb.rfid.notifier;
 
 // Connection IDs
 public interface NotificationSettings {
-    String SenderID = "51158287557";
-    String HubName = "AutoTAG";
+    String SenderID = "907965440312";
+    String HubName = "displayHub";
     String HubListenConnectionString =
-            "Endpoint=sb://autotag.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=8WyGVu9swcmkllMZj5yOEaYcGUhrUrKyY4ozzj+/s7U=";
-
-    String authSenderID = "796149648033";
-    String authHubName = "authorizationHub";
-    String authHubListenConnectionString =
-            "Endpoint=sb://authorizationhub.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=HRajUq1Wck/Il+LUCGxaF7Q9nCxG9N5lBbS3XzKFZ3Y=";
-
-    String dispSenderID = "907965440312";
-    String dispHubName = "displayHub";
-    String dispHubListenConnectionString =
             "Endpoint=sb://displayhub.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=gjtqKDTnYiyoHFklFYm8Xdy0jHXktRbrH2JOCbsgJbU=";
-
-
+    
 }
